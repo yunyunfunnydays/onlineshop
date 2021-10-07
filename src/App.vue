96b914184307cd1bcc2fabcb71d8c3f228c5a5c8
yunyunfunnydays <yunhsuan2017@gmail.com>
2021-10-07 09:14:51 +0800

    import bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,21 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// ~ 與 ../node_modules 意思相同，~ 指向 webpack 根目錄
+// 匯入自定義的 SCSS 元件，內容為客製化的 bootstrap，載入 SCSS 可忽略檔名類型
+@import "./assets/all";
 </style>
