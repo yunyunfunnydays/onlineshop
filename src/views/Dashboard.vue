4a4f9aec18ce867adef92e325f187def16ea2d48
yunyunfunnydays <yunhsuan2017@gmail.com>
2021-10-15 14:20:57 +0800

    get products list for dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,8 @@
 <template>
-  <!--新增 bootstrap navbar 元件，獨立為一個 components 檔案，並用註冊的方式匯入-->
-  <Navbar></Navbar>
-  <router-view/>
+    <Navbar></Navbar>
+    <div class="container-fluid">
+      <router-view/>
+    </div>
 </template>
 
 <script>
@@ -12,17 +13,12 @@ export default {
     Navbar,
   },
   created() {
-    // 從 cookie 取出 token
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
-    // 將 token 寫入 header，則每當 axios 發送 API 時，皆默認會發送此資訊
     this.axios.defaults.headers.common.Authorization = token;
-    // 建立接收驗證訊息的 API
     const api = `${process.env.VUE_APP_API}api/user/check`;
     console.log(api);
-    // 發送驗證訊息
     this.axios.post(api)
       .then((res) => {
-      // 當驗證結果為非登入中，畫面跳轉回登入畫面
         if (!res.data.success) {
           this.$router.push('/login');
         }
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,4 +1,65 @@
-// 產品頁
+// 後台管理，產品列表
 <template>
-  產品列
+  <table class="table mt-4">
+    <thead>
+      <tr>
+        <th width="120">分類</th>
+        <th>產品名稱</th>
+        <th width="120">原價</th>
+        <th width="120">售價</th>
+        <th width="100">是否啟用</th>
+        <th width="200">編輯</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- 用 v-for 建立有重複性的表格 -->
+      <tr v-for="item in products" :key="item.id">
+        <td>{{item.category}}</td>
+        <td>{{item.content}}</td>
+        <td class="text-right">{{item.origin_price}}</td>
+        <td class="text-right">{{item.price}}</td>
+        <td>
+          <!-- v-if 條件式 -->
+          <span class="text-success" v-if="item.is_enabled">啟用</span>
+          <span class="text-muted" v-else>未啟用</span>
+        </td>
+        <td>
+          <div class="btn-group">
+            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button class="btn btn-outline-danger btn-sm">刪除</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+      pagination: {}, // 分頁資訊
+    };
+  },
+  methods: {
+    // 後台管理，取得產品列表
+    getProducts() {
+      // 取得產品列表 API
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`;
+      this.axios.get(api)
+        .then((res) => {
+          if (res.data.success) {
+            // 取得後將資料存入 DATA，才可以帶入到 HTML
+            this.products = res.data.products;
+            this.pagination = res.data.pagination;
+          }
+        });
+    },
+  },
+  // 在頁面創建時觸發此方法取得產品列表
+  created() {
+    this.getProducts();
+  },
+};
+</script>
